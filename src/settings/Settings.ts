15e0101376e5b10276252b51c59625ef291628a8
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2025-09-24 18:18:23 +0200

    Store WIP

--- src/settings/Settings.ts
+++
@@ -1,7 +0,0 @@
-import {createWebHistory, NavigationGuardWithThis, RouteRecordRaw, RouterHistory} from "vue-router";
-
-export class Settings {
-    static routerHistory: RouterHistory = createWebHistory();
-    static routerRoutes: RouteRecordRaw[] = [];
-    static beforeEachRouteCallback?: NavigationGuardWithThis<any> = undefined;
-}
